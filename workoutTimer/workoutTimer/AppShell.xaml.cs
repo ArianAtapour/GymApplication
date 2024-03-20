f79acc36dbc588edc867336b2df257e949150295
Arian Atapour <arian.atapour@student.nhlstenden.com>
2024-03-20 12:31:38 +0100

    Timer demo

---
+++ workoutTimer/workoutTimer/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace workoutTimer;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ workoutTimer/workoutTimer/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace workoutTimer;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
