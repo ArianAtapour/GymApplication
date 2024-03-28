790936918641b9f0361c1260df311f0c7aedf48b
Arian Atapour <arian.atapour@student.nhlstenden.com>
2024-03-28 23:49:45 +0100

    Added checkbox for better selection and view of the workouts

--- WorkoutApp/WorkoutPage.xaml.cs
+++ WorkoutApp/WorkoutPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Maui.Controls;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 namespace WorkoutApp
@@ -16,10 +17,9 @@ namespace WorkoutApp
             exerciseListView.ItemsSource = exercises;
             workoutListView.ItemsSource = workouts;
 
-            workoutListView.ItemSelected += OnWorkoutItemSelected;
-
             workoutListView.SelectionMode = (ListViewSelectionMode)SelectionMode.Multiple;
         }
+
         private void OnAddExerciseClicked(object sender, EventArgs e)
         {
             string exerciseName = exerciseNameEntry.Text;
@@ -73,29 +73,82 @@ namespace WorkoutApp
                 return;
             }
 
-            
             var gymApplication = new GymApplication(selectedWorkouts);
             await Navigation.PushAsync(gymApplication);
         }
 
-        private void OnWorkoutItemSelected(object sender, SelectedItemChangedEventArgs e)
+        private void AddWorkoutToView(Workout workout)
         {
-            if (e.SelectedItem == null)
-                return;
+            var stackLayout = new StackLayout { Padding = new Thickness(5) };
 
-            var selectedWorkout = e.SelectedItem as Workout;
+            var checkBox = new CheckBox { VerticalOptions = LayoutOptions.Center };
 
-            if (!selectedWorkouts.Contains(selectedWorkout))
+            checkBox.CheckedChanged += (sender, e) =>
             {
-                selectedWorkouts.Add(selectedWorkout);
-            }
+                var chkBox = (CheckBox)sender;
+                var selectedWorkout = chkBox.BindingContext as Workout;
 
-            ((ListView)sender).SelectedItem = null;
+                if (e.Value && !selectedWorkouts.Contains(selectedWorkout))
+                {
+                    selectedWorkouts.Add(selectedWorkout);
+                }
+                else if (!e.Value && selectedWorkouts.Contains(selectedWorkout))
+                {
+                    selectedWorkouts.Remove(selectedWorkout);
+                }
+            };
+
+            var workoutNameLabel = new Label { Text = workout.WorkoutName, VerticalOptions = LayoutOptions.Center };
+            var durationLabel = new Label { Text = workout.TotalDuration.ToString(), VerticalOptions = LayoutOptions.Center };
+
+            stackLayout.Children.Add(checkBox);
+            stackLayout.Children.Add(workoutNameLabel);
+            stackLayout.Children.Add(durationLabel);
+
+            var exerciseListView = new ListView { ItemsSource = workout.Exercises };
+            exerciseListView.ItemTemplate = new DataTemplate(() =>
+            {
+                var exerciseNameLabel = new Label { VerticalOptions = LayoutOptions.Center };
+                exerciseNameLabel.SetBinding(Label.TextProperty, "Name");
+
+                var exerciseDurationLabel = new Label { VerticalOptions = LayoutOptions.Center };
+                exerciseDurationLabel.SetBinding(Label.TextProperty, "Duration");
+
+                return new ViewCell
+                {
+                    View = new StackLayout
+                    {
+                        Padding = new Thickness(5),
+                        Children =
+                {
+                    exerciseNameLabel,
+                    exerciseDurationLabel
+                }
+                    }
+                };
+            });
+
+            stackLayout.Children.Add(exerciseListView);
+
+            var viewCell = new ViewCell { View = stackLayout };
+            workoutListView.ItemTemplate = new DataTemplate(() => viewCell);
         }
 
 
+        private void OnCheckboxCheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            var checkbox = (CheckBox)sender;
+            var workout = (Workout)checkbox.BindingContext;
 
-
+            if (e.Value && !selectedWorkouts.Contains(workout))
+            {
+                selectedWorkouts.Add(workout);
+            }
+            else if (!e.Value && selectedWorkouts.Contains(workout))
+            {
+                selectedWorkouts.Remove(workout);
+            }
+        }
 
     }
 }
