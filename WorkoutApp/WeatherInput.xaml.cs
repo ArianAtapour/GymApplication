1fd6d07d38927acb129e824b3056c810e04e90a8
Nathan Mills <91469944+pepehonourable@users.noreply.github.com>
2024-03-29 14:56:57 +0100

    Second input Version

--- WorkoutApp/WeatherInput.xaml.cs
+++ WorkoutApp/WeatherInput.xaml.cs
@@ -15,15 +15,14 @@ public partial class WeatherInput : ContentPage
 
     private async void OnButtonClicked(object sender, EventArgs e)
     {
-        weatherData.Country = EntryCountry.Text;
-        weatherData.City = EntryCity.Text;
-
         if (string.IsNullOrEmpty(EntryCountry.Text) || string.IsNullOrEmpty(EntryCity.Text))
         {
             await DisplayAlert("Error", "Country and City cannot be empty", "OK");
         }
         else
         {
+            weatherData.Country = EntryCountry.Text;
+            weatherData.City = EntryCity.Text;
             await Shell.Current.Navigation.PushAsync(new WorkoutPage());
         }
     }
